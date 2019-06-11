239b6e2a8827696e4fac85c35bf7e0fa7a61a6bf
comunnidadoscar@gmail.com <comunidadoscar@gmail.com>
2019-06-11 16:29:08 -0400

    edits

--- js/script.js
+++ js/script.js
@@ -1,40 +1,54 @@
-// var sugguestionsSunny = ["sunglasses", "suncreen", "hat"];
+var sugguestionsSunny = ["sunglasses", "suncreen", "hat"];
 
-// var suggestionRain = ["umbrella", "rainboots", "rain poncho"];
+var suggestionRain = ["umbrella", "rainboots", "rain poncho"];
 
 
-
-// var length;
-// for (var count = 0; count < suggestionRain.length; count++) {
-//     $("#button2").append("<p> You should have " + suggestionRain[count] + " with you </p>");
-
-// }
-
+// $.ajax({
+//         url:"http://api.openweathermap.org/data/2.5/weather?q="+userInput+"&APPID=083239ab6de14c2379d19581628b2c16",
+//         type: "GET",
+//         success: function(suggestionRain) {
+//             $('body').append(suggestionRain.name, suggestionRain.weight, suggestionRain.height);
+//             $('body').append(suggestionsSunny.name, suggestionsSunny.weight, suggestionsSunny.height);
+//         }
+// )};
+let weatherResult;
+$("#button2").hide();
 $("#button1").click(function() {
     let userInput = $("#city").val();
-    const weatherIcons = [
-        "http://chittagongit.com/download/157274", // Thunder
-        "https://iconfree.net/uploads/icon/2017/8/29/rainy-weather-rain-icon-7502-512x512.png", // rain or drizzle
-        "https://image.flaticon.com/icons/png/512/53/53565.png", // Clear or sunny
-        "https://ui-ex.com/images/vector-sky-black-and-white-4.png", // partialcloudy
-        "http://icons.iconarchive.com/icons/icons8/ios7/256/Logos-Skydrive-Copyrighted-icon.png", //cloudy
-    ];
+    // const weatherIcons = [
+    //     "http://chittagongit.com/download/157274", // Thunder
+    //     "https://iconfree.net/uploads/icon/2017/8/29/rainy-weather-rain-icon-7502-512x512.png", // rain or drizzle
+    //     "https://image.flaticon.com/icons/png/512/53/53565.png", // Clear or sunny
+    //     "https://ui-ex.com/images/vector-sky-black-and-white-4.png", // partialcloudy
+    //     "http://icons.iconarchive.com/icons/icons8/ios7/256/Logos-Skydrive-Copyrighted-icon.png", //cloudy
+    // ];
 
 
     $.ajax({
         url:"http://api.openweathermap.org/data/2.5/weather?q="+userInput+"&APPID=083239ab6de14c2379d19581628b2c16",
         type: "GET",
         success: function(response) {
-            let weatherResult = response.weather[0].main;
+            weatherResult = response.weather[0].main;
+            console.log(weatherResult)
             let humidity = response.main.humidity;
 
             $("#result").html("<div>"+calculateFahrenhiet(response.main.temp)+"°F"+"</div>");
-            $("#result").append("<div>"+calculateFahrenhiet(response.main.temp_max)+"°F" +"</div>");
-            $("#result").append("<div>"+calculateFahrenhiet(response.main.temp_min) +"°F"+"</div>");
-
-            $("#result").append("<div>"+humidity+"%"+"</div>");
-            $("#result").append("<div>"+weatherResult+"</div>");
-            $("#result").append("<div>"+calculate_inHg( response.main.pressure)+" inHg"+"</div>");
+            $("#result").append("<div>"+"<p>High</p>"+calculateFahrenhiet(response.main.temp_max)+"°F" +"</div>");
+            $("#result").append("<div>"+"<p>Low</p>"+calculateFahrenhiet(response.main.temp_min) +"°F"+"</div>");
+            $("#result").append("<div>"+"<p>Humidity</p>"+humidity+"%"+"</div>");
+            $("#result").append("<div id='weather'>"+weatherResult+"</div>");
+            $("#result").append("<div>"+"<p>Pressure</p>"+calculate_inHg( response.main.pressure)+" inHg"+"</div>");
+            if(weatherResult === "Rain"){
+                $("#weather").append("<img src = 'https://iconfree.net/uploads/icon/2017/8/29/rainy-weather-rain-icon-7502-512x512.png'>")
+            }else if(weatherResult === "Clear" || weatherResult === "Sunny"){
+                $("#weather").append("<img src = 'https://image.flaticon.com/icons/png/512/53/53565.png'>")
+            }else if(weatherResult === "Clouds"){
+                $("#weather").append("<img src = 'http://icons.iconarchive.com/icons/icons8/ios7/256/Logos-Skydrive-Copyrighted-icon.png'>")
+            }else if(weatherResult === "Mist"){
+                $("#weather").append("<img src = 'https://image.flaticon.com/icons/png/512/130/130712.png'>")
+            }
+            $("#button2").show();
+            return weatherResult
         }
 
     });
@@ -62,4 +76,14 @@ function calculate_inHg(hPa){
     inHg = Math.floor(hPa)
     return  inHg
 }
+$("#button2").click(function(){
+    console.log(weatherResult);
+    if(weatherResult === "Rain"){
+        $("#suggestions").append("<h1>We suggest you bring</h1>")
+        suggestionRain.forEach(function(object){
+            $().append()
+        });
+    }
+
+});
 //http://api.openweathermap.org/data/2.5/weather?q=Manhattan&APPID=083239ab6de14c2379d19581628b2c16
